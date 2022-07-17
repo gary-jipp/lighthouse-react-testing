eb1a390c6d92eabf361b5e51442f5609dfbdea33
Gary J <github@accu.ca>
2022-07-17 15:02:06 -0700

    Merge

---
+++ src/helpers.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import uniqid from 'uniqid';
+
+export function getNewFriends(num) {
+  return axios.get(`https://random-data-api.com/api/name/random_name?size=${num}`)
+    .then(res => res.data);
+}
+
+export function getFriendCount(list) {
+  if (!list) {
+    return -1;
+  }
+
+  return list.length;
+}
+
+export function addFriend(list, name, uid = uniqid()) {
+  if (!list) {
+    return [];
+  }
+
+  // This code will mutate State
+  // list.push({name, uid});
+  // return list;
+
+  return [...list, {name, uid}];
+}
+
+export function removeFriend(list, uid) {
+  if (!list) {
+    return [];
+  }
+
+  return list.filter(item => item.uid !== uid);
+}
+
+export function getFriendNames(list) {
+  if (!list) {
+    return [];
+  }
+
+  return list.map(item => item.name);
+}
