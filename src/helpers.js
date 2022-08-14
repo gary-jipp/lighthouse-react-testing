39ef0c823659435c96eeca85b650027916a5dbf7
Gary J <github@accu.ca>
2022-08-14 14:14:29 -0700

    Update

--- src/helpers.js
+++ src/helpers.js
@@ -1,10 +1,5 @@
-import axios from 'axios';
 import uniqid from 'uniqid';
 
-export function getNewFriends(num) {
-  return axios.get(`https://random-data-api.com/api/name/random_name?size=${num}`)
-    .then(res => res.data);
-}
 
 export function getFriendCount(list) {
   if (!list) {
