47efc77a20b996126cbd4c5ed434941cc4b5241d
Gary J <github@accu.ca>
2022-08-14 13:49:07 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import {useState, useCallback, } from 'react';
 import Header from 'components/Header';
 import Input from 'components/Input';
 import FriendList from 'components/FriendList';
-import {addFriend, getNewFriends, removeFriend} from 'helpers';
+import axios from 'axios';
+import uniqid from 'uniqid';
 import 'App.css';
+import {getFriendCount} from 'helpers';
 
 export default function App() {
   const [data, setData] = useState([]);
   const newFriends = useCallback(() => {
-    getNewFriends(5).then(res => setData(res));
+    axios.get(`https://random-data-api.com/api/name/random_name?size=${5}`)
+      .then(res => setData(res));
   }, []);
 
 
@@ -21,14 +24,33 @@ export default function App() {
     setData(removeFriend(data, uid));
   };
 
+  function addFriend(list, name) {
+    if (!list) {
+      return [];
+    }
+
+    const uid = uniqid();
+    return [...list, {name, uid}];
+  }
+
+  function removeFriend(list, uid) {
+    if (!list) {
+      return [];
+    }
+
+    return list.filter(item => item.uid !== uid);
+  }
+
+  const title = `My Friends: ${getFriendCount(data)}`;
 
   return (
     <div className="App">
 
-      <Header text="My Friends" />
+      <Header text={title} />
       <Input onSave={addItem}></Input>
 
       <button type="button" onClick={newFriends}>Get New Friends</button>
+      <input type="checkbox" />
 
       <FriendList items={data} deleteItem={deleteItem} />
     </div>
--- src/helpers.js
+++ src/helpers.js
@@ -1,10 +1,3 @@
-import axios from 'axios';
-import uniqid from 'uniqid';
-
-export function getNewFriends(num) {
-  return axios.get(`https://random-data-api.com/api/name/random_name?size=${num}`)
-    .then(res => res.data);
-}
 
 export function getFriendCount(list) {
   if (!list) {
@@ -12,23 +5,6 @@ export function getFriendCount(list) {
   }
 
   return list.length;
-}
-
-export function addFriend(list, name) {
-  if (!list) {
-    return [];
-  }
-
-  const uid =  uniqid();
-  return [...list, {name, uid}];
-}
-
-export function removeFriend(list, uid) {
-  if (!list) {
-    return [];
-  }
-
-  return list.filter(item => item.uid !== uid);
 }
 
 export function getFriendNames(list) {
