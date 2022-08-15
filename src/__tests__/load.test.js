1234415f43aa9632fa20464e267bca2ee7854389
Gary J <github@accu.ca>
2022-08-14 18:06:19 -0700

    Update

--- src/__tests__/load.test.js
+++ src/__tests__/load.test.js
@@ -1,4 +1,4 @@
-const {getNewFriends} = require("helpers/fetch");
+const {loadNewFriends} = require("helpers/load");
 
 const data = [
   {name: "Betty White", uid: "1"},
@@ -10,20 +10,20 @@ const data = [
 describe('Helper Async tests', () => {
 
   //---------- ASYNC ----------------------
-  // Coverage - getNewFriends
-  test("getNewFriends returns 4 new friends", () => {
-    return getNewFriends(4)
+  // Coverage - loadNewFriends
+  test("loadNewFriends returns 4 new friends", () => {
+    return loadNewFriends(4)
       .then(friends => {
         expect(friends.length).toBe(4);
       });
   });
 
-  // Coverage - getNewFriends (async/await)
-  test("getNewFriends async/await returns 4 new friends", async () => {
+  // Coverage - loadNewFriends (async/await)
+  test("loadNewFriends async/await returns 4 new friends", async () => {
 
-    // const friends = getNewFriends(4);  // returns a promise
+    // const friends = loadNewFriends(4);  // returns a promise
 
-    const friends = await getNewFriends(4);  // returns res
+    const friends = await loadNewFriends(4);  // returns res
     expect(friends.length).toBe(4);
 
     // We can't really test much else
